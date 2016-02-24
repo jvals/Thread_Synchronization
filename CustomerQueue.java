afcf7ebb7583637bb047f985b5162bfdb8c5612a
jvals <jvals@users.noreply.github.com>
2016-02-24 15:08:04 +0100

    Add constructor for CustomerQueue

--- CustomerQueue.java
+++ CustomerQueue.java
@@ -4,19 +4,23 @@ import java.util.concurrent.LinkedBlockingQueue;
  * This class implements a queue of customers as a circular buffer.
  */
 public class CustomerQueue {
-    private LinkedBlockingQueue<Customer> customerQueue;
+	private LinkedBlockingQueue<Customer> customerQueue;
+	private int queueLength;
+	private Gui gui;
 
 	/**
 	 * Creates a new customer queue.
 	 * @param queueLength	The maximum length of the queue.
 	 * @param gui			A reference to the GUI interface.
 	 */
-    public CustomerQueue(int queueLength, Gui gui) {
-        // A blocking queue is a queue that support operations that wait
-        // for the queue to be non-empty, or that wait for space to become
-        // available. A LinkedBlockingQueue is an implementation of this
-        // based on linked nodes.
-		customerQueue = new LinkedBlockingQueue<Customer>(queueLength);
+	public CustomerQueue(int queueLength, Gui gui) {
+		// A blocking queue is a queue that support operations that wait
+		// for the queue to be non-empty, or that wait for space to become
+		// available. A LinkedBlockingQueue is an implementation of this
+		// based on linked nodes.
+		customerQueue = new LinkedBlockingQueue<>(queueLength);
+		this.queueLength = queueLength;
+		this.gui = gui;
 	}
 
 	// Add more methods as needed
