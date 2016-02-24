67f559d7be9c83cef583e57f167a4743c1c88d88
jvals <jvals@users.noreply.github.com>
2016-02-24 14:55:09 +0100

    add stopThread for doorman

--- Doorman.java
+++ Doorman.java
@@ -29,7 +29,7 @@ public class Doorman extends Thread {
 	 * Stops the doorman thread.
 	 */
 	public void stopThread() {
-		// Incomplete
+		threadIsRunning = false;
 	}
 
 	// Add more methods as needed
